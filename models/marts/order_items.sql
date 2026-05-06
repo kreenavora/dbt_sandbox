11d5daf32fdbbc0b3b8aeaf6dbdaa8804064d097
Kreena Vora <kreena.vora@dbtlabs.com>
2026-05-06 14:16:12 +0000

    fixed staging

--- models/marts/order_items.sql
+++ models/marts/order_items.sql
@@ -10,7 +10,6 @@ line_item as (
 
 )
 select
-
     {{ dbt_utils.generate_surrogate_key(['line_item.order_key', 'line_item.line_number']) }} as order_item_key,
     orders.order_key,
     orders.customer_key,
--- models/staging/stg_tpch_parts.sql
+++ models/staging/stg_tpch_parts.sql
@@ -1,1 +1,19 @@
-select * from {{ source ('tpch_sf001', 'part')}}
+with source as (
+    select * from {{ source('tpch_sf001', 'part') }}
+),
+
+renamed as (
+    select
+        p_partkey     as part_key,
+        p_name        as part_name,
+        p_mfgr        as manufacturer,
+        p_brand       as brand,
+        p_type        as part_type,
+        p_size        as part_size,
+        p_container   as container,
+        p_retailprice as retail_price,
+        p_comment     as part_comment
+    from source
+)
+
+select * from renamed
